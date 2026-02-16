9242703111d13370ffa2b379d5470f313c2bc98d
JSLY <2029872426@qq.com>
2026-02-16 16:16:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.message_components import Node,Plain   
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("caipiao", "J-SLY", "一个虚拟货币系统", "1.0.0")
+class user:
+    id:str
+    menoy:int
+    today_is_buy_cp:bool
+    today_cp_num:list=[[],[]]
+    def __init__(self,id:str):
+        self.id=id
+        self.menoy=1000
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,15 +19,37 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command_group("vc")
+    def vc(self):
+        pass
+    @vc.command("help",alias={'帮助'})
+    async def help(self, event: AstrMessageEvent):
+        help_text=Node(
+            content=[
+                Plain("帮助文档：\n\n /vc help /vc 帮助 ：输出本帮助列表\n")
+            ]
+        )
+        yield event.chain_result([help_text])
+    @vc.command("rg",alias={'注册'})
+    async def rg(self,event:AstrMessageEvent):
+        if await self.get_kv_data(f"{event.get_sender_id()}", False) == False:
+            tmp_user:user=user(event.get_sender_id())
+            await self.put_kv_data(f"{event.get_sender_id()}",tmp_user) # type: ignore
+            yield event.plain_result("注册成功，初始资金：1000")
+        else:
+            yield event.plain_result(f"您{event.get_sender_id()}已经注册！")
 
+    @vc.command("me",alias={'个人主页'})
+    async def me(self,event:AstrMessageEvent):
+        person = await self.get_kv_data(f"{event.get_sender_id()}",False)
+        if  person== False:
+            yield event.plain_result("您未注册！请使用/vc rg进行注册")
+        else:
+            person_info=Node(
+                content=[
+                    Plain(f"{event.get_sender_id}的个人主页\n\nID：{event.get_sender_id()}\n余额：{event.get_sender_id()}")
+                ]
+            )
+            yield event.chain_result([person_info])
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
